23664b2c2cc78da2c480f23bd17dff0fb9701f50
sslshuanglong <sslshuanglong@163.com>
2019-12-12 11:29:03 +0800

    new topic

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
-from .models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Board, Topic, Post
 
 # Create your views here.
 
@@ -15,5 +15,24 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'new_topic.html', {'board':board})
+    
+    if request.method == 'POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
 
+        user = User.objects.first()
+
+        topic = Topic.objects.create(
+            subject=subject,
+            board=board,
+            starter=user
+        )
+
+        post = Post.objects.create(
+            message=message,
+            topic=topic,
+            created_by=user
+        )
+
+        return redirect('board_topics', pk=board.pk)
+    return render(request, 'new_topic.html', {'board':board})
